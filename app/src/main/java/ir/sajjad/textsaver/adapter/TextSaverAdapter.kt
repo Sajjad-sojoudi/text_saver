4e19f6360cd888a831669bb15eeb230ae69cbb1b
Sajjad <sajad.soojoodi@gmail.com>
2023-12-20 17:35:24 +0330

    add ItemView long click

--- app/src/main/java/ir/sajjad/textsaver/adapter/TextSaverAdapter.kt
+++ app/src/main/java/ir/sajjad/textsaver/adapter/TextSaverAdapter.kt
@@ -1,16 +1,20 @@
 package ir.sajjad.textsaver.adapter
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.view.LayoutInflater
 import android.view.TextureView
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import ir.sajjad.textsaver.R
 import ir.sajjad.textsaver.data.ItemTextPost
+import java.text.FieldPosition
 
-class TextSaverAdapter(private val data :List<ItemTextPost>):RecyclerView.Adapter<TextSaverAdapter.TextSaverViewHolder>() {
+class TextSaverAdapter(private val data: ArrayList<ItemTextPost>, private val textEvent: TextEvent) : RecyclerView.Adapter<TextSaverAdapter.TextSaverViewHolder>() {
+
     inner class TextSaverViewHolder(itemView: View , private val context: Context):RecyclerView.ViewHolder(itemView){
         val txtTitle = itemView.findViewById<TextView>(R.id.txt_title)
         val txtDetails = itemView.findViewById<TextView>(R.id.txt_details)
@@ -18,6 +22,15 @@ class TextSaverAdapter(private val data :List<ItemTextPost>):RecyclerView.Adapte
         fun bindData(position: Int){
             txtTitle.text = data[position].txtTitle
             txtDetails.text = data[position].txtDetails
+
+            itemView.setOnClickListener {
+                textEvent.onTextClicked()
+            }
+
+            itemView.setOnLongClickListener{
+                textEvent.onTextLongClicked(data[position],adapterPosition)
+                true
+            }
         }
     }
 
@@ -34,4 +47,23 @@ class TextSaverAdapter(private val data :List<ItemTextPost>):RecyclerView.Adapte
         holder.bindData(position)
     }
 
+    @SuppressLint("NotifyDataSetChanged")
+    fun setData(newData: ItemTextPost) {
+        data.add(newData)
+        notifyItemInserted(0)
+
+    }
+
+    fun removeData(oldData : ItemTextPost , oldPosition : Int){
+        data.remove(oldData)
+        notifyItemRemoved(oldPosition)
+    }
+
+    interface TextEvent{
+        fun onTextClicked()
+        fun onTextLongClicked(textPost: ItemTextPost,pos : Int)
+    }
+
 }
+
+
